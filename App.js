034ab2493b577b8cac042f816ce93a64abf52df4
ramon33li <ramonalvessantos36@gmail.com>
2022-09-30 23:22:27 +0000

    gasolinha ou alcool

--- App.js
+++ App.js
@@ -1,11 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState}  from'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
 
 export default function App() {
+		const [alcool,setAlcool]  = useState("2.19");
+		const [gaso, setGaso]     = useState("3.35");
+		const [resultado,setResultado] = useState("");
+	
+		function dividir(){
+		let r = parseFloat(alcool) / parseFloat(gaso);
+			
+			
+			if(r >= 0.7){
+				setResultado('gasolina' +r.toFixed(2)) 
+			}else {
+				setResultado('alcool' +r.toFixed(2))
+			}		
+	}
+	
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <View style={styles.bloco}>
+	 <Text style={styles.titulo}>gasolinha</Text>	  
+		  </View>
+		  <View style={styles.bloco}>
+		  <Text> gasolinha:</Text>
+		  <TextInput
+			  style={styles.Input}
+			  onChangeText={(valor)=>setGaso(valor)}
+			  value={gaso}
+			  KeyboardType="numeric"
+			  />
+		  </View>
+		   <View style={styles.bloco}>
+	 <Text style={styles.titulo}>alcool</Text>	  
+		  </View>
+		  <View style={styles.bloco}>
+		  <Text> alcool:</Text>
+		  <TextInput
+			  style={styles.Input}
+			  onChangeText={(valor)=>setAlcool(valor)}
+			  value={alcool}
+			  KeyboardType="numeric"
+			   />
+		  </View>
+		  
+		  
+		  <View style={styles.bloco}>
+		  <TouchableOpacity
+			  style={styles.botao}
+			  onPress={dividir}
+			  >
+			  <Text style={styles.textbotao}>calcular</Text>
+			  </TouchableOpacity>
+		  </View>
+		  
+		  <View style={styles.bloco}>
+		  <Text style={styles.titulo}>
+			 Resultado:{resultado}
+			  </Text>
+		  </View>
     </View>
   );
 }
@@ -13,8 +67,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+	
+ bloco:{
+	  marginTop:20,
+	  width:'80%',
+	  marginLeft:'10%',
+		 color:'#7000000'
+  },
+  titulo:{
+	  fontSize:30,
+	  textAlign:'center',
+	  backgroundColorColor:'#000000'
+  },
+  Input:{
+	borderWidth:2,
+	fontSize:20,
+	borderRadius:10,
+	  cursor: 'pointe',
+	  backgroundColor:'#fff',
+	  borderRadius:10,
+	  fontfamily: 'cursive'
+  },
+  botao:{
+	  backgroundColor:'#696969',
+	  borderRadius:10,
+	  fontSize:20,
+	  fontfamily: 'cursive'
+	  
+  },
+  textoBotao:{
+	  color:'#696969',
+	  textAlign:'center',
+	  fontSize:20,
+  }		
 });
